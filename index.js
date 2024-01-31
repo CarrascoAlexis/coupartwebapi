fe4d9d5b26814066bd568139d66fa076e546658c
CarrascoAlexis <acarrasco@gaming.tech>
2024-01-31 10:15:57 +0100

    connect to DB

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = process.env.PORT || 5100;
+
+const config = require('./config')
+
+const db = mysql.createConnection(config.db)
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
