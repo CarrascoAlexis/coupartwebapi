322825403c3485045dc317e14196ca0b25997ad3
CarrascoAlexis <acarrasco@gaming.tech>
2024-01-31 10:25:36 +0100

    localhost switch for test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     db: {
       /* don't expose password or any sensitive info, done only for demo */
-      host: "acarrasco.fr",
+      host: "localhost",
       user: "coupartweb",
       password: "Titouan426!",
       database: "coupartweb",
